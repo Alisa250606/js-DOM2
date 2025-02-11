d32795023a682c5bc1205d1ea44e00bd0034a8ea
Alisa250606 <kn23-a.daieva@nubip.edu.ua>
2025-02-11 09:38:42 +0200

    LAB10

---
+++ src/module/gallery.js
@@ -0,0 +1,29 @@
+import { fetchImages } from './api.js';
+
+const gallery = document.getElementById('gallery');
+
+export async function loadImages(count) {
+  const images = await fetchImages(count);
+  images.forEach(image => {
+    const imgElement = document.createElement('img');
+    imgElement.src = image.download_url;
+    imgElement.alt = `Photo by ${image.author}`;
+    gallery.appendChild(imgElement);
+  });
+}
+
+export function clearGallery() {
+  gallery.innerHTML = '';
+}
+
+export function removeLastImage() {
+  if (gallery.lastElementChild) {
+    gallery.removeChild(gallery.lastElementChild);
+  }
+}
+
+export function reverseGallery() {
+  const images = Array.from(gallery.children);
+  gallery.innerHTML = '';
+  images.reverse().forEach(img => gallery.appendChild(img));
+}
